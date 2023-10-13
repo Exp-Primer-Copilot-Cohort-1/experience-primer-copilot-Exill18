35208579ea876ecae6542e70f0cd507a83373009
Francisco <108736956+Exill18@users.noreply.github.com>
2023-10-13 09:08:42 +0000

    Copilot third  commit

--- comments.js
+++ comments.js
@@ -5,8 +5,9 @@ const port = 3000;
 
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello, World!\n');
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html><body><h1>Hello, World!</h1></body></html>');
+    res.end();
 });
 
 server.listen(port, hostname, () => {
